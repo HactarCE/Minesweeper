1156311a526a0db94187b4810a3ebf9113736ce4
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-09-22 21:03:19 -0400

    Add Tetra game template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(dead_code)]
+
+use tetra::graphics::{self, Color};
+use tetra::{Context, ContextBuilder, State};
+
+struct GameState;
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context, _dt: f64) -> tetra::Result {
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+        Ok(())
+    }
+}
+
+fn main() -> tetra::Result {
+    ContextBuilder::new("Hello, world!", 1280, 720)
+        .show_mouse(true)
+        .build()?
+        .run(&mut GameState)
 }
